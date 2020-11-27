67d7edff251f89bf1a53cc572e26beb3594dda28
Ben <benjamin.cloquet@gmail.com>
2020-11-27 23:52:01 +0100

    Remove logs

--- services/ytdl.js
+++ services/ytdl.js
@@ -7,10 +7,8 @@ exports.isValidYoutubeURL = async (url) => ytdl.validateURL(url);
 exports.getVideoTitle = async (url) => {
   try {
     const videoInfo = await ytdl.getBasicInfo(url);
-    console.log(videoInfo.videoDetails);
     return videoInfo.videoDetails.title;
   } catch (err) {
-    console.log(err);
     return null;
   }
 };
